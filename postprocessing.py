6ba31533d8dff304a491383907ad1e1e4fadd75b
gondalali777 <110658744+gondalali777@users.noreply.github.com>
2022-08-05 09:57:45 +0200

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -4,7 +4,7 @@ from feedgen.feed import FeedGenerator
 from datetime import datetime, timezone
 
 # Normal GitHub Pages URL
-# ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
+# ghPagesURL = "https://github.com/gondalali777/tiktok-rss-RedPak/"
 
 # Custom Domain
 ghPagesURL = "https://github.com/gondalali777/tiktok-rss-RedPak/"
